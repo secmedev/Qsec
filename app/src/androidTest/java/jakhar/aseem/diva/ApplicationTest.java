d5d52a4ddb4db604474b0009fe24b7c3351559ed
secmedev <121214790+secmedev@users.noreply.github.com>
2026-08-15 14:59:18 +0530

    Update ApplicationTest.java

--- app/src/androidTest/java/jakhar/aseem/diva/ApplicationTest.java
+++ app/src/androidTest/java/jakhar/aseem/diva/ApplicationTest.java
@@ -3,7 +3,7 @@ package jakhar.aseem.diva;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
+/**testt
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
